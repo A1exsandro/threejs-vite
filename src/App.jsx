fc25b3463c1503f3cfa8a7abc8580fd54ac9f06a
A1exsandro <alexsandro.acp@gmail.com>
2023-02-12 20:44:38 -0400

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,20 @@
 import { useEffect } from 'react'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
+import SceneInit from './lib/SceneInit'
 
 import './App.css'
 
 function App() {
   useEffect(() => {
-    const scene = new THREE.Scene()
-
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1, 1000
-    )
-    camera.position.z = 96
-
-    const canvas = document.getElementById('myThreeJsCanvas')
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true
-    })
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-
-    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
-    ambientLight.castShadow = true
-    scene.add(ambientLight)
-
-    const spotLight = new THREE.SpotLight(0xFFFFFF, 1)
-    spotLight.castShadow = true
-    spotLight.position.set(0, 64, 32)
-    scene.add(spotLight)
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
 
     // ADD BOX
     const boxGeometry = new THREE.BoxGeometry(16, 16, 16)
     const boxMaterial = new THREE.MeshNormalMaterial()
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
-    scene.add(boxMesh)
-
-    // ADD ORBIT CONTROLS
-    const controls = new OrbitControls(camera, renderer.domElement)
-
-    // ADD FPS STATS
-    const stats = Stats()
-    document.body.appendChild(stats.dom)
-
-    const animate = () => {
-      boxMesh.rotation.x += 0.01
-      boxMesh.rotation.y += 0.01
-      renderer.render(scene, camera)
-      window.requestAnimationFrame(animate)
-    }
-    animate()
+    test.scene.add(boxMesh)
 
   },[])
 
