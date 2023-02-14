2f8ac20541c3b36b9c1d32e1d075ace3f5469258
A1exsandro <alexsandro.acp@gmail.com>
2023-02-14 15:51:29 -0400

    create class ThreeExperience

---
+++ src/components/Script.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+class ThreeExperience {
+  container = null
+  camera = null
+  renderer = null
+
+  controls = null
+
+  constructor() {}
+
+  initScene() {}
+
+  render() {}
+
+  resize() {}
+
+  cleanUp() {}
+}
+
+export { ThreeExperience }
